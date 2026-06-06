d3f24677cc262db05f2348ea6fcd3ebfbe041716
Meet Patel <meetpatel.dev45@gmail.com>
2026-06-06 15:25:27 +0530

    remove legacy JS files and clean up project assets

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "swiper/css";
+import "remixicon/fonts/remixicon.css";
 import { BrowserRouter } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import App from "./App";
 
